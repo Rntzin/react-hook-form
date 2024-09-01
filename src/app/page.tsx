019afe4bc2f50ed0126b275b512f7835e74ed001
Andre <kinglemoyra@gmail.com>
2024-08-31 21:06:42 -0300

    feat: useForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Box,
   Button,
@@ -9,8 +11,47 @@ import {
   Text,
   Textarea,
 } from "@chakra-ui/react";
+import { FunctionComponent, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
-export default function Home() {
+interface UserFormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  address: string;
+  phone: string;
+  description: string;
+  onSubmit: () => void;
+}
+
+const schema = z.object({
+  firstName: z.string().nonempty("required"),
+  lastName: z.string().nonempty("required"),
+  email: z.string().email("must be a valid email").nonempty("required"),
+  address: z.string().nonempty("required"),
+  phone: z.string().nonempty("required"),
+  description: z.string().nonempty("required"),
+});
+
+const Home: FunctionComponent = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserFormData>({
+    resolver: zodResolver(schema),
+  });
+
+  function onSubmit(data: UserFormData) {
+    console.log(data);
+  }
+
+  const setErros = (error: any) => {
+    console.log("Errors", error);
+  };
+
   return (
     <Flex
       minHeight="100vh"
@@ -34,103 +75,107 @@ export default function Home() {
           <Text fontSize="2xl">Registration Form</Text>
         </Heading>
         <Box>
-          <Flex justify="space-between">
-            <FormControl isRequired marginTop="15px" width="49%">
-              <FormLabel>First name</FormLabel>
+          <form action="" onSubmit={handleSubmit(onSubmit, setErros)}>
+            <Flex justify="space-between">
+              <FormControl isRequired marginTop="15px" width="49%">
+                <FormLabel>First name</FormLabel>
+                <Input
+                  type="fistName"
+                  border="none"
+                  outline="none"
+                  bgColor="gray.900"
+                  focusBorderColor="gray.600"
+                  color="gray.200"
+                  placeholder="First Name"
+                  {...register("firstName")}
+                />
+              </FormControl>
+              <FormControl isRequired marginTop="15px" width="49%">
+                <FormLabel>Last name</FormLabel>
+                <Input
+                  type="lastName"
+                  border="none"
+                  outline="none"
+                  bgColor="gray.900"
+                  focusBorderColor="gray.600"
+                  color="gray.200"
+                  placeholder="Last Name"
+                  {...register("lastName")}
+                />
+              </FormControl>
+            </Flex>
+            <FormControl isRequired marginTop="15px">
+              <FormLabel>Your e-mail</FormLabel>
               <Input
                 type="text"
-                name="name"
                 border="none"
                 outline="none"
                 bgColor="gray.900"
                 focusBorderColor="gray.600"
                 color="gray.200"
-                placeholder="First Name"
+                placeholder="Your e-mail"
+                {...register("email")}
               />
             </FormControl>
-            <FormControl isRequired marginTop="15px" width="49%">
-              <FormLabel>Last name</FormLabel>
-              <Input
-                type="text"
-                name="name"
+            <Flex justify="space-between">
+              <FormControl isRequired marginTop="15px" width="49%">
+                <FormLabel>Your address</FormLabel>
+                <Input
+                  type="text"
+                  border="none"
+                  outline="none"
+                  bgColor="gray.900"
+                  focusBorderColor="gray.600"
+                  color="gray.200"
+                  placeholder="Your address"
+                  {...register("address")}
+                />
+              </FormControl>
+              <FormControl isRequired marginTop="15px" width="49%">
+                <FormLabel>Your phone</FormLabel>
+                <Input
+                  type="text"
+                  border="none"
+                  outline="none"
+                  bgColor="gray.900"
+                  focusBorderColor="gray.600"
+                  color="gray.200"
+                  placeholder="Your phone"
+                  {...register("phone")}
+                />
+              </FormControl>
+            </Flex>
+            <FormControl isRequired marginTop="15px">
+              <FormLabel>Your Description</FormLabel>
+              <Textarea
                 border="none"
                 outline="none"
                 bgColor="gray.900"
                 focusBorderColor="gray.600"
                 color="gray.200"
-                placeholder="Last Name"
+                resize="none"
+                placeholder="Your description"
+                {...register("description")}
               />
             </FormControl>
-          </Flex>
-          <FormControl isRequired marginTop="15px">
-            <FormLabel>Your e-mail</FormLabel>
-            <Input
-              type="text"
-              name="name"
-              border="none"
-              outline="none"
-              bgColor="gray.900"
-              focusBorderColor="gray.600"
+            <Button
+              type="submit"
+              width="full"
+              bgColor="green.400"
+              mt="4px"
               color="gray.200"
-              placeholder="Your e-mail"
-            />
-          </FormControl>
-          <Flex justify="space-between">
-            <FormControl isRequired marginTop="15px" width="49%">
-              <FormLabel>Your address</FormLabel>
-              <Input
-                type="text"
-                name="name"
-                border="none"
-                outline="none"
-                bgColor="gray.900"
-                focusBorderColor="gray.600"
-                color="gray.200"
-                placeholder="Your address"
-              />
-            </FormControl>
-            <FormControl isRequired marginTop="15px" width="49%">
-              <FormLabel>Your phone</FormLabel>
-              <Input
-                type="text"
-                name="name"
-                border="none"
-                outline="none"
-                bgColor="gray.900"
-                focusBorderColor="gray.600"
-                color="gray.200"
-                placeholder="Your phone"
-              />
-            </FormControl>
-          </Flex>
-          <FormControl isRequired marginTop="15px">
-            <FormLabel>Your descript message</FormLabel>
-            <Textarea
-              name="name"
-              border="none"
-              outline="none"
-              bgColor="gray.900"
-              focusBorderColor="gray.600"
-              color="gray.200"
-              resize="none"
-              placeholder="Your descript message"
-            />
-          </FormControl>
-          <Button
-            type="submit"
-            width="full"
-            bgColor="green.400"
-            mt="4px"
-            color="gray.200"
-            _hover={{
-              bgColor: "green.600",
-              color: "gray.700",
-            }}
-          >
-            Register
-          </Button>
+              _hover={{
+                bgColor: "green.600",
+                color: "gray.700",
+              }}
+            >
+              Register
+            </Button>
+          </form>
         </Box>
       </Box>
     </Flex>
   );
-}
+};
+
+export default Home;
